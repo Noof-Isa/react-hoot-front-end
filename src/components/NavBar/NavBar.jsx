664aaa78030080e3ce3494bf8537e4e5f7c0d08f
Noof-Isa <noofisa786@gmail.com>
2025-08-07 11:21:42 +0300

    init commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,37 @@
+// to cheack  the nav bar place 
+
+// const NavBar = ({ user }) => {
+//   return (
+//     <>
+//       <p>Hello Navbar</p>
+//     </>
+//   )
+// }
+// export default NavBar
+
+import { Link } from 'react-router-dom'
+
+const NavBar = (props) => {
+
+
+
+  return (
+    <nav>
+      {props.user ? (
+        <ul>
+          <li>Welcome {props.user.username}</li>
+          <li><Link to="/"> Home </Link></li>
+          <li><Link to='/' onClick={props.handleSignOut}>Sign Out</Link></li>
+        </ul>
+        ) : (
+          <ul>
+            <li><Link to="/sign-up">Sign Up</Link></li>
+          
+                      <li><Link to="/sign-in">Sign In</Link></li>
+</ul>
+          ) }
+    </nav>
+  )
+}
+
+export default NavBar 
---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+const SignUp = (props) => {
+const navigate = useNavigate()
+
+const initialState = {
+    username: '',
+    password: '',
+        passwordConf: '',
+
+  }
+
+  const [formData, setFormData] = useState(initialState)
+
+  const handleChange = (evt) => {
+    setFormData({...formData, [evt.target.name]: evt.target.value})
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    console.log('form was submitted')
+    props.handleSignUp(formData)
+        navigate('/')
+
+  }
+  let formIsInvalid = true
+
+  if (formData.username && formData.password && formData.password === formData.passwordConf) {
+    formIsInvalid = false
+  }
+  return (
+    <main>
+      <h1>Sign up Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Username:</label>
+        <input type="text" name='username' onChange={handleChange} />
+        <br />
+        <label>Password:</label>
+        <input type="password" name='password' onChange={handleChange} />
+        <br />
+                <label>Confirm Password:</label>
+        <input type="password" name="passwordConf" onChange={handleChange} />
+        <br />
+        <button type="submit" disabled={formIsInvalid}>Sign up</button>
+
+      
+      </form>
+    </main>
+  )
+}
+
+export default SignUp
